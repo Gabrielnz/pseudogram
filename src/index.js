985bf680419cc3994bbc86b426fdc46051b057dc
Gabriel Nunez <gabriel.nunez.sme@outlook.com>
2017-03-26 10:47:25 -0400

    #02 integrando firebase en la app y anadiendo autenticacion social

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import firebase from 'firebase'
 import App from './App'
 import './index.css'
+
+// inicializando firebase con la configuracion necesaria
+firebase.initializeApp({
+  apiKey: 'AIzaSyDjFgnKOcyqO1yGCxUAQiCwZQnThkf4hy8',
+  authDomain: 'pseudogram-b71a6.firebaseapp.com',
+  databaseURL: 'https://pseudogram-b71a6.firebaseio.com',
+  storageBucket: 'pseudogram-b71a6.appspot.com',
+  messagingSenderId: '1088358105829'
+})
 
 ReactDOM.render(
   <App />,
