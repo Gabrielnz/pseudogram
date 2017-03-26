dfc2a4bee4c811bcb2f4ee5c6de1d1c9bf4dbecc
Gabriel Nunez <gabriel.nunez.sme@outlook.com>
2017-03-26 16:49:38 -0400

    #04 Almacenando datos con Firebase Realtime Database

--- src/App.js
+++ src/App.js
@@ -7,16 +7,25 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      user: null
+      user: null,
+      pictures: [],
+      uploadValue: 0
     }
 
     this.handleAuth = this.handleAuth.bind(this)
     this.handleLogout = this.handleLogout.bind(this)
+    this.handleUpload = this.handleUpload.bind(this)
   }
 
-  componentWillMount () {
+  componentDidMount () {
     firebase.auth().onAuthStateChanged(user => {
       this.setState({ user })
+    })
+
+    firebase.database().ref('pictures').on('child_added', snapshot => {
+      this.setState({
+        pictures: this.state.pictures.concat(snapshot.val())
+      })
     })
   }
 
@@ -28,7 +37,19 @@ class App extends Component {
           <img width='200' src={this.state.user.photoURL} alt={this.state.user.displayName} />
           <p>Hola {this.state.user.displayName}!</p>
           <button onClick={this.handleLogout}>Salir</button>
-          <FileUpload />
+          <FileUpload onUpload={this.handleUpload} />
+
+          {
+            this.state.pictures.map((picture, index) => (
+              <div key={index}>
+                <img src={picture.image} alt='' />
+                <br />
+                <img width='100' src={picture.photoURL} alt={picture.displayName} />
+                <br />
+                <span>{picture.displayName}</span>
+              </div>
+            )).reverse()
+          }
         </div>
       )
     } else {
@@ -58,6 +79,37 @@ class App extends Component {
     })
     .catch(err => {
       console.log(`Error - ${err.code}: ${err.message}`)
+    })
+  }
+
+  // evento que maneja la carga de una imagen
+  handleUpload (event) {
+    const file = event.target.files[0]
+    const storageRef = firebase.storage().ref(`/fotos/${file.name}`)
+    const task = storageRef.put(file)
+    console.log('entra a handleUpload')
+
+    // cuando el estado de la carga de la imagen cambia
+    task.on('state_changed', snapshot => {
+      let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+
+      this.setState({ uploadValue: percentage })
+      console.log(this.state.uploadValue)
+    }, err => {
+      // cuando ocurre un error en la carga de la imagen
+      console.log(err.message)
+    }, () => {
+      // cuando la carga de la imagen finaliza
+      const record = {
+        photoURL: this.state.user.photoURL,
+        displayName: this.state.user.displayName,
+        image: task.snapshot.downloadURL
+      }
+
+      const dbRef = firebase.database().ref('pictures')
+      const newPicture = dbRef.push()
+      newPicture.set(record)
+      console.log('termina la carga')
     })
   }
 
--- src/FileUpload.js
+++ src/FileUpload.js
@@ -1,39 +1,12 @@
 import React, { Component } from 'react'
-import firebase from 'firebase'
 
 class FileUpload extends Component {
   constructor () {
     super()
 
     this.state = {
-      uploadValue: 0,
-      picture: null
+      uploadValue: 0
     }
-
-    this.handleUpload = this.handleUpload.bind(this)
-  }
-
-  // evento que maneja la carga de una imagen
-  handleUpload (event) {
-    const file = event.target.files[0]
-    const storageRef = firebase.storage().ref(`/fotos/${file.name}`)
-    const task = storageRef.put(file)
-
-    // cuando el estado de la carga de la imagen cambia
-    task.on('state_changed', snapshot => {
-      let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-
-      this.setState({ uploadValue: percentage })
-    }, err => {
-      // cuando ocurre un error en la carga de la imagen
-      console.log(err.message)
-    }, () => {
-      // cuando la carga de la imagen finaliza
-      console.log(task.snapshot)
-      this.setState({
-        picture: task.snapshot.downloadURL
-      })
-    })
   }
 
   render () {
@@ -41,9 +14,7 @@ class FileUpload extends Component {
       <div>
         <progress value={this.state.uploadValue} max='100' />
         <br />
-        <input type='file' onChange={this.handleUpload} />
-        <br />
-        <img width='320' src={this.state.picture} alt='' />
+        <input type='file' onChange={this.props.onUpload} />
       </div>
     )
   }
